ef602efe46f1972c06a5a0bec70222eb5484666d
@sontnhe161855 <sontnhe161855@fpt.edu.vn>
2023-10-27 17:26:07 +0700

    update

--- src/main/java/com/example/birdfarmprojectbe/api/FoodNormAPI.java
+++ src/main/java/com/example/birdfarmprojectbe/api/FoodNormAPI.java
@@ -1,6 +1,8 @@
 package com.example.birdfarmprojectbe.api;
 
+import com.example.birdfarmprojectbe.api.error.BadRequestAlertException;
 import com.example.birdfarmprojectbe.models.Bird;
+import com.example.birdfarmprojectbe.models.BirdType;
 import com.example.birdfarmprojectbe.models.FoodNorm;
 import com.example.birdfarmprojectbe.repository.FoodNormRepository;
 import com.example.birdfarmprojectbe.service.FoodNormService;
@@ -9,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Objects;
 
 @CrossOrigin(value = "*", allowedHeaders = "*")
 @RestController
@@ -19,6 +22,8 @@ public class FoodNormAPI {
 
     private final FoodNormService foodNormService;
 
+    private final String ENTITY_NAME = "FoodNormAPI";
+
     @PostMapping("/create")
     public ResponseEntity<FoodNorm> create(@RequestBody FoodNorm foodNorm){
         return ResponseEntity.ok(foodNormRepository.save(foodNorm));
@@ -28,4 +33,20 @@ public class FoodNormAPI {
     public ResponseEntity<List<FoodNorm>> get(@RequestParam(value = "birdTypeID",required = false) Integer birdTypeID){
         return ResponseEntity.ok(foodNormService.get(birdTypeID));
     }
+
+    @PostMapping("/update/{id}")
+    public ResponseEntity<FoodNorm> update(@PathVariable final Integer id,@RequestBody FoodNorm foodNorm){
+        if (!Objects.equals(id, foodNorm.getId())) {
+            throw new BadRequestAlertException("Invalid ID", ENTITY_NAME, "idinvalid");
+        }
+
+        if (!foodNormRepository.existsById(id)) {
+            throw new BadRequestAlertException("Entity not found", ENTITY_NAME, "idnotfound");
+        }
+        FoodNorm result = foodNormRepository.save(foodNorm);
+        return ResponseEntity
+                .ok()
+                .body(result);
+    }
+
 }
